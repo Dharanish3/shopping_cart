0a8666e4056fda7393834f6ca4edf929ece16316
Dharanish3 <dharanishsk9698@gmail.com>
2023-12-21 23:53:27 +0530

    Cart Task

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Topbar from './Component/Topbar'
+import Header from './Component/Header'
+import Card from './Component/Card'
+import Footer from './Component/Footer'
+
+function App() {
+  return (
+    <>
+    <Topbar/>
+    <Header/>
+    <Card/>
+    <Footer/>
+    </>
+   
+  )
+}
+
+export default App
