108c8565fb0763155269be52a147538178f1b720
Dharanish3 <dharanishsk9698@gmail.com>
2023-12-23 23:59:45 +0530

    Cart Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React ,{useState}  from 'react'
 import Topbar from './Component/Topbar'
 import Header from './Component/Header'
 import Card from './Component/Card'
 import Footer from './Component/Footer'
 
 function App() {
+  const [cart , setCart] = useState(0)
+
+   
   return (
     <>
-    <Topbar/>
+    <Topbar cart={cart} setCart={setCart}/>
     <Header/>
-    <Card/>
+    <Card cart={cart} setCart={setCart}  />
     <Footer/>
     </>
    
