f75f4d1d80d55fc2a73ad35ac3104d810ee4cd6f
Dharanish <120582836+Dharanish3@users.noreply.github.com>
2024-01-31 13:22:02 +0530

    Update Card.jsx

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -32,9 +32,9 @@ function Card({cart , setCart }) {
             <div className="container px-4 px-lg-5 mt-5">
                 <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                   
-                   {items.map((item , index) => {
+                   {items.map((item , i) => {
                     return (<>
-                        <div className="col mb-5" key={index}>
+                        <div className="col mb-5" key={i}>
                         <div className="card h-100">
                            
                             
